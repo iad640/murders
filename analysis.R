59b1bcbd064239567f607b524f723b4bec20bda8
iad640 <ivan.alonso@uib.es>
2021-11-23 17:31:37 +0100

    Report is added

--- analysis.R
+++ analysis.R
@@ -3,3 +3,4 @@ load("rdas/murders.rda")#Note we use relative path to the working directory to a
 
 murders %>% mutate(abb = reorder(abb,rate)) %>% ggplot(aes(abb, rate)) + geom_bar(width=0.5, stat="identity", color = "red") + coord_flip()
 ggsave("figs/barplot.png") #Saves the generate image in a folder called figs
+
